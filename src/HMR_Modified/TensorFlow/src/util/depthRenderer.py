c0973f0b844ea32e3385eee92f8dcbf37293b3c2
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-03-19 23:55:08 +0400

    bugfix / Upgrade to Python 3

--- src/HMR_Modified/TensorFlow/src/util/depthRenderer.py
+++ src/HMR_Modified/TensorFlow/src/util/depthRenderer.py
@@ -220,7 +220,7 @@ def render_model(verts,
     if color_id is None:
         color = colors['light_blue']
     else:
-        color_list = colors.values()
+        color_list = list(colors.values())
         color = color_list[color_id % len(color_list)]
 
     # color = np.array([255, 255, 255])
@@ -295,19 +295,19 @@ def draw_skeleton(input_image, joints, draw_edges=True, vis=None, radius=None):
         radius = max(4, (np.mean(input_image.shape[:2]) * 0.01).astype(int))
 
     colors = {
-        'pink': np.array([197, 27, 125]),  # L lower leg
-        'light_pink': np.array([233, 163, 201]),  # L upper leg
-        'light_green': np.array([161, 215, 106]),  # L lower arm
-        'green': np.array([77, 146, 33]),  # L upper arm
-        'red': np.array([215, 48, 39]),  # head
-        'light_red': np.array([252, 146, 114]),  # head
-        'light_orange': np.array([252, 141, 89]),  # chest
-        'purple': np.array([118, 42, 131]),  # R lower leg
-        'light_purple': np.array([175, 141, 195]),  # R upper
-        'light_blue': np.array([145, 191, 219]),  # R lower arm
-        'blue': np.array([69, 117, 180]),  # R upper arm
-        'gray': np.array([130, 130, 130]),  #
-        'white': np.array([255, 255, 255]),  #
+        'pink': (197, 27, 125),  # L lower leg
+        'light_pink': (233, 163, 201),  # L upper leg
+        'light_green': (161, 215, 106),  # L lower arm
+        'green': (77, 146, 33),  # L upper arm
+        'red': (215, 48, 39),  # head
+        'light_red': (252, 146, 114),  # head
+        'light_orange': (252, 141, 89),  # chest
+        'purple': (118, 42, 131),  # R lower leg
+        'light_purple': (175, 141, 195),  # R upper
+        'light_blue': (145, 191, 219),  # R lower arm
+        'blue': (69, 117, 180),  # R upper arm
+        'gray': (130, 130, 130),  #
+        'white': (255, 255, 255),  #
     }
 
     image = input_image.copy()
@@ -355,23 +355,8 @@ def draw_skeleton(input_image, joints, draw_edges=True, vis=None, radius=None):
             18: 'light_green',
             14: 'purple'
         }
-    elif joints.shape[1] == 19:
-        parents = np.array([
-            1,
-            2,
-            8,
-            9,
-            3,
-            4,
-            7,
-            8,
-            -1,
-            -1,
-            9,
-            10,
-            13,
-            -1,
-        ])
+    elif joints.shape[1] == 14:
+        parents = np.array([ 1, 2, 8, 9, 3, 4, 7, 8, -1, -1, 9, 10, 13, -1])
         ecolors = {
             0: 'light_pink',
             1: 'light_pink',
@@ -390,21 +375,16 @@ def draw_skeleton(input_image, joints, draw_edges=True, vis=None, radius=None):
         import ipdb
         ipdb.set_trace()
 
-    for child in xrange(len(parents)):
+    for child in range(len(parents)):
         point = joints[:, child]
         # If invisible skip
         if vis is not None and vis[child] == 0:
             continue
         if draw_edges:
-            cv2.circle(image, (point[0], point[1]), radius, colors['white'],
-                       -1)
-            cv2.circle(image, (point[0], point[1]), radius - 1,
-                       colors[jcolors[child]], -1)
+            cv2.circle(image, (point[0], point[1]), radius, colors['white'],-1)
+            cv2.circle(image, (point[0], point[1]), radius - 1, colors[jcolors[child]], -1)
         else:
-            # cv2.circle(image, (point[0], point[1]), 5, colors['white'], 1)
-            cv2.circle(image, (point[0], point[1]), radius - 1,
-                       colors[jcolors[child]], 1)
-            # cv2.circle(image, (point[0], point[1]), 5, colors['gray'], -1)
+            cv2.circle(image, (point[0], point[1]), radius - 1, colors[jcolors[child]], 1)
         pa_id = parents[child]
         if draw_edges and pa_id >= 0:
             if vis is not None and vis[pa_id] == 0:
