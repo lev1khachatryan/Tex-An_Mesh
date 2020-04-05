8fa7577901553a6a55668da1c39ded5b226341c4
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-04-05 16:09:42 +0400

    story / Move test runner

--- src/PIFu/scripts/test.sh
+++ src/PIFu/scripts/test.sh
@@ -16,13 +16,13 @@ MLP_DIM_COLOR='513 1024 512 256 128 3'
 # NOTE: one can change here to reconstruct mesh in a different resolution.
 VOL_RES=256
 
-CHECKPOINTS_NETG_PATH='checkpoints/net_G'
-CHECKPOINTS_NETC_PATH='checkpoints/net_C'
+CHECKPOINTS_NETG_PATH='./checkpoints/net_G'
+CHECKPOINTS_NETC_PATH='./checkpoints/net_C'
 
-TEST_FOLDER_PATH='sample_images'
+TEST_FOLDER_PATH='./sample_images'
 
 # command
-CUDA_VISIBLE_DEVICES=${GPU_ID} python apps/eval.py \
+CUDA_VISIBLE_DEVICES=${GPU_ID} python3 ./apps/eval.py \
     --name ${NAME} \
     --batch_size ${BATCH_SIZE} \
     --mlp_dim ${MLP_DIM} \
@@ -36,8 +36,3 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python apps/eval.py \
     --test_folder_path ${TEST_FOLDER_PATH} \
     --load_netG_checkpoint_path ${CHECKPOINTS_NETG_PATH} \
     --load_netC_checkpoint_path ${CHECKPOINTS_NETC_PATH}
-
-function pause(){
-   read -p "$*"
-}
-pause 'Press [Enter] key to continue...'
