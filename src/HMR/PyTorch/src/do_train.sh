8dd147eb93089d87bff1ccadebc4ac2694b5100f
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-05-29 00:49:42 +0400

    story / delete unused files

--- src/HMR/PyTorch/src/do_train.sh
+++
@@ -1,5 +0,0 @@
-
-
-#CUDA_VISIBLE_DEVICES=4,5,6,7 python3 trainer.py
-
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python3 trainer.py > train.log &
--- src/HMR/TensorFlow/pre-trained.sh
+++
@@ -1,1 +0,0 @@
-wget https://people.eecs.berkeley.edu/~kanazawa/cachedir/hmr/models.tar.gz && tar -xf models.tar.gz
